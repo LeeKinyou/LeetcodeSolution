49d6eb8bdca5035ef28cde734b43e17e9e783aa1
Lee Kinyou <1412901165@qq.com>
2025-10-30 10:30:26 +0800

    提交主站题目1526

---
+++ src/main/java/com/cqupt/leetcode/mainstation/M1526/AnswerTest.java
@@ -0,0 +1,13 @@
+package com.cqupt.leetcode.mainstation.M1526;
+
+import org.junit.Test;
+
+public class AnswerTest {
+    @Test
+    public void test() {
+        Solution solution = new Solution();
+        int[] nums = new int[]{3, 1, 5, 4, 2};
+        int result = solution.minNumberOperations(nums);
+        System.out.println(result);
+    }
+}
