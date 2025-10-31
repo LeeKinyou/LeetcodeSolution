f59fe41fb331628b3e42a41a00994cbe0f7c00cb
Lee Kinyou <1412901165@qq.com>
2025-10-31 18:21:00 +0800

    添加LCR题目023

---
+++ src/main/java/com/cqupt/leetcode/LCR/LCR023/AnswerTest.java
@@ -0,0 +1,9 @@
+package com.cqupt.leetcode.LCR.LCR023;
+
+import org.junit.Test;
+
+public class AnswerTest {
+    @Test
+    public void test() {
+    }
+}
---
+++ src/main/java/com/cqupt/leetcode/LCR/LCR023/ListNode.java
@@ -0,0 +1,11 @@
+package com.cqupt.leetcode.LCR.LCR023;
+
+public class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+        next = null;
+    }
+}
